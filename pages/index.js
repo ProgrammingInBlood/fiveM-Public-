64a452d2f1251f79be456a43e39463c7e92a6881
Eklavya Singh <eklavyasingh12065@gmail.com>
2021-12-26 01:43:04 +0530

    added hand-detection 26-12-21

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,126 @@ import styles from "../styles/Homepage.module.scss";
 export default function Home() {
   const router = useRouter();
   const [showModal, setShowModal] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
+  const [cameraPermission, setCameraPermission] = useState(false);
+  const [url, setUrl] = useState("");
 
   const handleModel = () => {
-    setShowModal(!showModal);
+    if (showModal) {
+      setShowModal(false);
+      document.body.style.overflow = "visible";
+    } else {
+      setShowModal(true);
+      document.body.style.overflow = "hidden";
+    }
   };
+
+  const handleModel2 = () => {
+    if (showModal2) {
+      setShowModal2(false);
+      document.body.style.overflow = "visible";
+    } else {
+      setShowModal2(true);
+      document.body.style.overflow = "hidden";
+    }
+  };
+
+  const handleRoute = (path) => {
+    setUrl(path);
+    if (cameraPermission == true) {
+      router.push(path);
+    } else {
+      handleModel2();
+      setShowModal(true);
+    }
+  };
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) =>
+        devices.forEach((device) =>
+          device.label != "" ? setCameraPermission(true) : null
+        )
+      );
+  }, []);
+
+  console.log({ cameraPermission });
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Unreal Eye - Argumented Reality</title>
+        <meta property="og:site_name" content="UnReal Eye"></meta>
+        <meta property="og:url" content="https://eklavyasingh.me/"></meta>
+        <meta
+          property="og:image"
+          content="https://www.facebook.com/images/fb_icon_325x325.png"
+        ></meta>
         <meta name="theme-color" content="#000"></meta>
       </Head>
       <Navbar />
+      <div
+        className={styles.modalContainer}
+        style={{
+          visibility: showModal2 ? "visible" : "hidden",
+          opacity: showModal2 ? 1 : 0,
+        }}
+      >
+        <div className={styles.modal}>
+          <h1>Choose a Model</h1>
+          <ul>
+            <li>
+              <div
+                className={styles.modalItems}
+                onClick={() => handleRoute("/object-detection")}
+              >
+                <Image src="/images/1.png" width={100} height={100} />
+                <h3>Object Detection</h3>
+              </div>
+
+              <div className={styles.modalItems}>
+                <Image src="/images/2.png" width={100} height={100} />
+                <h3>Text Toxicity Detection</h3>
+              </div>
+            </li>
+            <li>
+              <div
+                className={styles.modalItems}
+                onClick={() => handleRoute("/hand-detection")}
+              >
+                <Image src="/images/3.png" width={100} height={100} />
+                <h3>Hand Detection</h3>
+              </div>
+
+              <div className={styles.modalItems}>
+                <Image src="/images/4.png" width={100} height={100} />
+                <h3>Body Segmentation</h3>
+              </div>
+            </li>
+
+            <li>
+              <div className={styles.modalItems}>
+                <Image src="/images/5.png" width={100} height={100} />
+                <h3>Face Detection</h3>
+              </div>
+
+              <div className={styles.modalItems}>
+                <Image src="/images/6.png" width={100} height={100} />
+                <h3>KNN Classifier</h3>
+              </div>
+            </li>
+          </ul>
+          <div
+            className={styles.buttons}
+            style={{ justifyContent: "flex-end" }}
+          >
+            <button className={styles.button} onClick={handleModel2}>
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
       <div
         className={styles.modalContainer}
         style={{
@@ -27,8 +135,9 @@ export default function Home() {
           opacity: showModal ? 1 : 0,
         }}
       >
-        <div className={styles.modal}>
+        <div className={styles.modal} style={{ paddingTop: 30 }}>
           <div className={styles.close}></div>
+
           <h1>We Need Camera Access To Work</h1>
           <p style={{ color: "#73717e", paddingTop: 10 }}>
             Please Allow camera permission to continue{" "}
@@ -51,10 +160,7 @@ export default function Home() {
             className={styles.buttons}
             style={{ justifyContent: "flex-end" }}
           >
-            <button
-              className={styles.button}
-              onClick={() => router.push("/object-detection")}
-            >
+            <button className={styles.button} onClick={() => router.push(url)}>
               Continue
             </button>
             <button className={styles.button} onClick={handleModel}>
@@ -72,7 +178,7 @@ export default function Home() {
             computer-generated perceptual information.
           </p>
           <div className={styles.buttons}>
-            <button className={styles.button} onClick={handleModel}>
+            <button className={styles.button} onClick={handleModel2}>
               Launch AR
             </button>
             <button className={styles.button}>Learn More</button>
